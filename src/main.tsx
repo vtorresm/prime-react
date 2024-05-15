858831de01ecd27e20cf629bb62b017cdadc70fe
Victor Torres <vatorresm88@gmail.com>
2024-05-15 13:53:25 -0500

    Terminando la config de Prime React

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,21 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+import 'primeicons/primeicons.css';
+import { PrimeReactProvider } from 'primereact/api';
+import Tailwind from 'primereact/passthrough/tailwind';
+import ThemeSwitcher from './components/themeSwitcher.tsx';
+import 'primereact/resources/themes/lara-light-indigo/theme.css';
+
+import './assets/index.css';
+import './assets/flags.css';
+import App from './App.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <PrimeReactProvider value={{ unstyled: true, pt: Tailwind }}>
+      <ThemeSwitcher />
+      <App />
+    </PrimeReactProvider>
+  </React.StrictMode>
+);
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+  darkMode: 'class',
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/primereact/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
