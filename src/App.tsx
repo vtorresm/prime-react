8ae2e48805ff2bf507321faa47c4a5495acb9329
Victor Torres <vatorresm88@gmail.com>
2024-05-17 13:54:43 -0500

    Probando configuración

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,57 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Panel } from 'primereact/panel';
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function TailwindDemo() {
+    return (
+        <div
+        className="
+          bg-white
+          dark:bg-gray-800
+          p-10
+          rounded-xl
+          flex flex-col
+          gap-8
+          max-w-3xl
+        "
+      >
+        <h1 className="text-4xl text-black dark:text-white font-bold text-center">
+          Tailwind CSS + PrimeReact
+        </h1>
+        <Panel header="Default Preset">
+          <p>
+            First panel component uses the global pass through preset from the
+            Tailwind CSS based implementation of PrimeOne Design 2023.
+          </p>
+        </Panel>
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <Panel
+          header="Custom Header"
+          pt={{
+            header:
+              'p-5 flex items-center justify-between border border-indigo-300 bg-indigo-500 text-indigo-50 rounded-tl-lg rounded-tr-lg dark:bg-indigo-900 dark:border-indigo-900/40 dark:text-white/80',
+          }}
+        >
+          <p>
+            Second panel overrides the header section with custom a custom style.
+          </p>
+        </Panel>
+
+        <Panel
+          header="Custom Design"
+          ptOptions={{ mergeSections: false }}
+          pt={{
+            header:
+              'flex items-center justify-center p-5 bg-gradient-to-r from-rose-400 via-fuchsia-500 to-indigo-500 rounded-tl-2xl rounded-tr-2xl text-white',
+            title: 'leading-none font-bold uppercase text-2xl',
+            content:
+              'bg-gradient-to-r from-rose-400 via-fuchsia-500 to-indigo-500 px-5 pb-8 pt-3 text-white text-center rounded-bl-2xl rounded-br-2xl text-xl',
+          }}
+        >
+          <p>
+            Third panel ignores the default preset with
+            <b> mergeSections: false</b> and applies a custom style to all elements
+            of the panel instead.
+          </p>
+        </Panel>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    )
 }
-
-export default App
